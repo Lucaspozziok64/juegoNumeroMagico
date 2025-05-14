d9a02fe0c4705d22d424b25c67bc9a96aa0ddaeb
Lucasfi <lukafigueroa64@gmail.com>
2025-05-13 21:30:07 -0300

    logramos capturar numero random mediante funcion y otra funcion para comparar el valor ingresado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
+let randomNumber;
+
+const numeroRandom = () => {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber);
+    return randomNumber
+}
+
+const compararNumero = () => {
+    const numeroIngresado = document.getElementById('inputNumero');
+    const inputValue = numeroIngresado.value;
+    const numeroParseado = parseInt(inputValue, 10);
+    console.log(numeroParseado);
+
+    if(numeroParseado > randomNumber) {
+        alert('El numero que ingresaste es MAYOR al numero MAGICO')
+    } else if (numeroParseado < randomNumber) {
+        alert('El numero ingresado es MENOR al numero MAGICO')
+    } else {
+        alert('¡¡¡ADIVINASTE!!!');
+    }
+}
+
+
 
 
