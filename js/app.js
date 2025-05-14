159079db517ac5dbf2e9f790895d7f680e3f6282
Lucasfi <lukafigueroa64@gmail.com>
2025-05-13 22:34:54 -0300

    agregamos loigca para desabilitar el boton de comenzar una vez clickeado y modificamos rango juego a 10

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 let randomNumber;
 
 const numeroRandom = () => {
-    randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(randomNumber);
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    alert('Numero generado(1-10)...adivina cual es!!!');
     return randomNumber
 }
 
@@ -10,7 +10,6 @@ const compararNumero = () => {
     const numeroIngresado = document.getElementById('inputNumero');
     const inputValue = numeroIngresado.value;
     const numeroParseado = parseInt(inputValue, 10);
-    console.log(numeroParseado);
 
     if(numeroParseado > randomNumber) {
         alert('El numero que ingresaste es MAYOR al numero MAGICO')
@@ -20,6 +19,11 @@ const compararNumero = () => {
         alert('¡¡¡ADIVINASTE!!!');
     }
 }
+
+const botonComenzar = document.getElementById('botonComenzar')
+botonComenzar.addEventListener('click', function() {
+    this.disabled = true;
+})
 
 
 
